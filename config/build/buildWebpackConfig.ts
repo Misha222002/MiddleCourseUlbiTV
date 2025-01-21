2d45e90e740e4fc409ba4b216d8c9f531736d6ce
Misha222002 <work_of_misha@mail.ru>
2025-01-21 18:40:04 +0300

    add-react-and-css-module

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -3,12 +3,11 @@ import { buildLoaders } from "./buildLoaders";
 import { buildPlugin } from "./buildPlugins";
 import { buildResolvers } from "./buildResolvers";
 import { BuildOptions } from "./types/config";
-import path from 'path';
 import webpack from 'webpack';
 
 export function buildWebpackConfig(options: BuildOptions):webpack.Configuration{
     const {mode, paths, isDev} = options
-    console.log(isDev, mode)
+
     return {
         mode,
         entry: paths.entry,
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client';
+import { Counter } from './components/Counter';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<div><Counter/></div>);
