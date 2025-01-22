13d066ca25328ca79324bd873a681ded6fbe1d73
Misha222002 <work_of_misha@mail.ru>
2025-01-22 14:37:34 +0300

    configurate-css-module

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,16 +1,19 @@
-import { useState } from "react"
+import { useState } from "react";
+import * as classes from "./Counter.module.scss";
 
 export const Counter = () => {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
     const increment = () => {
-        setCount(count+1)
-    }
+        setCount(count + 1);
+    };
+
+    console.log(classes); // Добавьте это для отладки
 
     return (
-        <div>
+        <div className={classes.btn}>
             {count}
             <button onClick={increment}>increment</button>
         </div>
-    )
-}
+    );
+};
