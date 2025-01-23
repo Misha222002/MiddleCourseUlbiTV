333470f87a4e7e0b0294bbc68013b12f5c5d0321
Misha222002 <work_of_misha@mail.ru>
2025-01-23 13:10:55 +0300

    les12_navbar_firstUI-kit

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,34 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import * as style from "./AppLink.module.scss";
+import { Link, LinkProps } from "react-router-dom";
+import { FC, ReactNode } from "react";
+
+export enum AppLinkTheme {
+  PRIMARY = "primary",
+  INVERTED = "inverted",
+}
+
+interface AppLinkProps extends LinkProps {
+  className?: string;
+  children: ReactNode;
+  theme?: AppLinkTheme;
+}
+
+export const AppLink: FC<AppLinkProps> = (props) => {
+  const {
+    to,
+    className,
+    children,
+    theme = AppLinkTheme.PRIMARY,
+    ...otherProps
+  } = props;
+  return (
+    <Link
+      to={to}
+      className={classNames(style.appLink, {}, [className, style[theme]])}
+      {...otherProps}
+    >
+      {children}
+    </Link>
+  );
+};
