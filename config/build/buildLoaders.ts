c6ace79ae96a911c4c354d57e5ca02c25c73b67b
Misha222002 <work_of_misha@mail.ru>
2025-01-24 18:02:53 +0300

    les13_SVGLoaderPngLoader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,33 +1,42 @@
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import webpack from 'webpack';
-import { BuildOptions } from './types/config';
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
 
-export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[]{
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            {
-                loader: 'css-loader',
-                options:{
-                    modules: {
-                        auto: true,
-                        localIdentName:isDev?"[path][name]__[local]--[hash:base64:8]":"[hash:base64:8]"
-                    }, 
-                }
-            },
-            "sass-loader",
-        ],
-    }
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const svgLoader = {
+    test: /\.svg$/,
+    use: ["@svgr/webpack"],
+  };
 
-    const typeScriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      }
+  const filesLoader = {
+    test: /\.png|jpg|gif/,
+    type: "asset/resource",
+  };
 
-    return [
-        typeScriptLoader,
-        cssLoader
-      ]
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: true,
+            localIdentName: isDev
+              ? "[path][name]__[local]--[hash:base64:8]"
+              : "[hash:base64:8]",
+          },
+        },
+      },
+      "sass-loader",
+    ],
+  };
+
+  const typeScriptLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
+
+  return [filesLoader, svgLoader, typeScriptLoader, cssLoader];
 }
