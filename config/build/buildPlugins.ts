4e6e900cebd0d21cd8f2811dc1f619c0e3b21307
Misha222002 <work_of_misha@mail.ru>
2025-01-31 16:24:37 +0300

    les23_bundleAnylizer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import webpack from "webpack";
 import { BuildOptions } from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 export function buildPlugin({
     paths,
@@ -18,6 +19,9 @@ export function buildPlugin({
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
+        }),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
         }),
     ].filter(Boolean);
 }
