05ec98058f515c32a94a05cdc1bfb6db6678a1a7
Misha222002 <work_of_misha@mail.ru>
2025-02-06 16:42:29 +0300

    add screenshot test

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -9,6 +9,5 @@ describe("button", () => {
     test("button clear theme", () => {
         render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
         expect(screen.getByText("TEST")).toHaveClass("clear");
-        screen.debug();
     });
 });
