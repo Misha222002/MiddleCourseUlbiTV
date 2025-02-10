edff24f1d6d0aa9b83a34e7aa0e9a1039564458e
Misha222002 <work_of_misha@mail.ru>
2025-02-11 00:19:52 +0300

    add

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -3,13 +3,7 @@ import { useTranslation } from "react-i18next";
 
 function MainPage() {
     const { t } = useTranslation();
-    return (
-        <div>
-            {t("Главная страница")}
-
-            <Counter />
-        </div>
-    );
+    return <div>{t("Главная страница")}</div>;
 }
 
 export default MainPage;
